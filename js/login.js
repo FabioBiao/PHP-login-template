2c92abf733f4c77b283d2f31d12b5f2b077b1f33
FabioBiao <fabio_madeira@hotmail.com>
2017-10-07 16:29:15 +0100

    optimizing some stuff

--- js/login.js
+++ js/login.js
@@ -7,9 +7,11 @@ $(document).ready(function () {
         if ((username === "") || (password === "")) {
             $("#message").html("<div class=\"alert alert-danger alert-dismissable\"><button type=\"button\" class=\"close\" data-dismiss=\"alert\" aria-hidden=\"true\">&times;</button>Please enter a username and a password</div>");
         } else {
+            console.log(username);
+            console.log(password);
             $.ajax({
                 type: "POST",
-                url: "checklogin.php",
+                url: "validate-login.php",
                 data: "myusername=" + username + "&mypassword=" + password,
                 dataType: 'JSON',
                 success: function (html) {
@@ -25,6 +27,7 @@ $(document).ready(function () {
                 error: function (textStatus, errorThrown) {
                     console.log(textStatus);
                     console.log(errorThrown);
+                    $("#message").html();
                 },
                 beforeSend: function () {
                     $("#message").html("<p class='text-center'><img src='images/ajax-loader.gif'></p>");
--- login/config.php
+++ login/config.php
@@ -1,6 +1,11 @@
 <?php
-//Pull '$base_url' and '$signin_url' from this file
-include 'globalcon.php';
+
+//SYSTEM SETTINGS
+$base_url = 'http://' . $_SERVER['SERVER_NAME'];
+$signin_url = substr($base_url . $_SERVER['PHP_SELF'], 0, -(6 + strlen(basename($_SERVER['PHP_SELF']))));
+//DO NOT CHANGE
+$ip_address = $_SERVER['REMOTE_ADDR'];
+
 //Pull database configuration from this file
 include 'dbconf.php';
 //Set this for global site use
