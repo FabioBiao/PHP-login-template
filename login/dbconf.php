26d279a3ed0bdf40b35f5193d1d75eee33333596
FabioBiao <fabio_madeira@hotmail.com>
2017-10-07 15:26:53 +0100

    register user update
    register user update

--- login/dbconf.php
+++ login/dbconf.php
@@ -1,7 +1,7 @@
 <?php
 //DATABASE CONNECTION VARIABLES
 $host = "localhost"; // Host name
-$username = "user"; // Mysql username
+$username = "root"; // Mysql username
 $password = ""; // Mysql password
 $db_name = "login"; // Database name
 //DO NOT CHANGE BELOW THIS LINE UNLESS YOU CHANGE THE NAMES OF THE MEMBERS AND LOGINATTEMPTS TABLES
